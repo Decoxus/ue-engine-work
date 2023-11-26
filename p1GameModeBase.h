7f4aaceb2fd41844fd400029a0122d5b64097421
Decoxus <121085563+Decoxus@users.noreply.github.com>
2023-11-26 12:25:57 +0800

    Add files via upload

---
+++ p1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "p1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class P1_API Ap1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
